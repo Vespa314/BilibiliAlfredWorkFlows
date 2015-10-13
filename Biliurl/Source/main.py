40caaa0f0521de7e3cce174485f3d1a0b3c5412c
Vespa314 <kylensherlock@gmail.com>
2015-10-13 20:41:59 +0800

    更新下载MP4视频

--- Biliurl/Source/main.py
+++ Biliurl/Source/main.py
@@ -9,11 +9,14 @@ Created on Mon May 26 23:42:03 2014
 from support import *
 from Feedback import *
 
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.99 Safari/537.36'
+APPKEY = '85eb6835b0a1034e'
+APPSEC = '2ad42749773c441109bdc0191257a664'
+
 default_encoding = 'utf-8'
 if sys.getdefaultencoding() != default_encoding:
     reload(sys)
     sys.setdefaultencoding(default_encoding)
-
 
 def GetVideoInfo(aid, appkey,page = 1, AppSecret=None, fav = None):
     """
@@ -29,61 +32,62 @@ def GetVideoInfo(aid, appkey,page = 1, AppSecret=None, fav = None):
     url =  'http://api.bilibili.cn/view?'+GetSign(paras,appkey,AppSecret)
     jsoninfo = JsonInfo(url)
     video = Video(aid,jsoninfo.Getvalue('title'))
-    video.guankan = jsoninfo.Getvalue('play')
-    video.commentNumber = jsoninfo.Getvalue('review')
-    video.danmu = jsoninfo.Getvalue('video_review')
-    video.shoucang = jsoninfo.Getvalue('favorites')
-    video.description = jsoninfo.Getvalue('description')
-    video.tag = []
-    taglist = jsoninfo.Getvalue('tag')
-    if taglist:
-        for tag in taglist.split(','):
-            video.tag.append(tag)
-    video.cover = jsoninfo.Getvalue('pic')
-    video.author = User(jsoninfo.Getvalue('mid'),jsoninfo.Getvalue('author'))
-    video.page = jsoninfo.Getvalue('pages')
-    video.date = jsoninfo.Getvalue('created_at')
-    video.credit = jsoninfo.Getvalue('credit')
-    video.coin = jsoninfo.Getvalue('coins')
-    video.spid = jsoninfo.Getvalue('spid')
     video.cid = jsoninfo.Getvalue('cid')
-    video.offsite = jsoninfo.Getvalue('offsite')
-    video.partname = jsoninfo.Getvalue('partname')
-    video.src = jsoninfo.Getvalue('src')
-    video.tid = jsoninfo.Getvalue('tid')
-    video.typename = jsoninfo.Getvalue('typename')
-    video.instant_server = jsoninfo.Getvalue('instant_server')
-    ## 以下三个意义不明。。
-    # video.allow_bp = jsoninfo.Getvalue('allow_bp')
-    # video.allow_feed = jsoninfo.Getvalue('allow_feed')
-    # video.created = jsoninfo.Getvalue('created')
     return video
 
-def GetBilibiliUrl(url, appkey, AppSecret=None):
+def GetBilibiliUrl(url):
+    global video
     overseas=False
     url_get_media = 'http://interface.bilibili.com/playurl?' if not overseas else 'http://interface.bilibili.com/v_cdn_play?'
     regex_match = re.findall('http:/*[^/]+/video/av(\\d+)(/|/index.html|/index_(\\d+).html)?(\\?|#|$)',url)
     if not regex_match:
-        return []
+        raise ValueError('Invalid URL: %s' % url)
     aid = regex_match[0][0]
     pid = regex_match[0][2] or '1'
-    video = GetVideoInfo(aid,appkey,pid,AppSecret)
+    cid_args = {'type': 'json', 'id': aid, 'page': pid}
+
     cid = video.cid
-    media_args = {'cid': cid,'quality':4}
-    resp_media = getURLContent(url_get_media+GetSign(media_args,appkey,AppSecret))
-    media_urls = [str(k.wholeText).strip() for i in xml.dom.minidom.parseString(resp_media.decode('utf-8', 'replace')).getElementsByTagName('durl') for j in i.getElementsByTagName('url')[:1] for k in j.childNodes if k.nodeType == 4]
+    media_args = {'otype': 'json', 'cid': cid, 'type': 'mp4', 'quality': 4, 'appkey': APPKEY}
+    resp_media = getURLContent(url_get_media+GetSign(media_args,APPKEY,APPSEC))
+    resp_media = dict(json.loads(resp_media.decode('utf-8', 'replace')))
+    media_urls = resp_media.get('durl')
+    media_urls = media_urls[0]
+    media_urls = media_urls.get('url')
     return media_urls
+    
+def GetSign(params,appkey,AppSecret=None):
+    params['appkey']=appkey;
+    data = "";
+    paras = sorted(params)
+    paras.sort();
+    for para in paras:
+        if data != "":
+            data += "&";
+        data += para + "=" + str(params[para]);
+    if AppSecret == None:
+        return data
+    m = hashlib.md5()
+    m.update((data+AppSecret).encode('utf-8'))
+    return data+'&sign='+m.hexdigest()
+
+def ChangeFuck(params):
+    data = "";
+    paras = params;
+    for para in paras:
+        if data != "":
+            data += "&";
+        data += para + "=" + str(params[para]);
+    return data
 
 fb = Feedback()
 url = '{query}'
-# url = 'http://www.bilibili.com/video/av2527767/'
-appkey = "03fc8eb101b091fb"
+# url = 'http://www.bilibili.com/video/av3033822/'
 av = GetRE(url,r"\d+")[0]
-video = GetVideoInfo(av,appkey)
+video = GetVideoInfo(av,appkey=APPKEY,AppSecret=APPSEC)
+downloadUrl = GetBilibiliUrl(url)
 # print video.title
-downloadUrl = GetBilibiliUrl(url,appkey)
 # print downloadUrl
 
-for item in downloadUrl:
-    fb.add_item('获取'+url+'下载地址',subtitle=video.title,arg=item)
+
+fb.add_item('获取'+url+'下载地址',subtitle=video.title,arg=downloadUrl)
 print fb
